0a678e46b7260698c7d727cc8f5fc6b130a4f63c
Sean Li <reachsean@gmail.com>
2019-12-07 00:51:41 -0800

    Updated notes from tonight

--- hello.py
+++ hello.py
@@ -11,3 +11,11 @@ viking_song = "Spam " * spam_amount
 print (viking_song)
 
 print("Hello World")
+
+# Before I got this file to work, I had to re-clone using SSH key
+# So next I had to learn how to create an SSH key on my Mac
+# After that, I installed the SSH key onto my GitHub
+# Then, I got stuck for 15-20 minutes getting an "Git: host key verification failed" error
+# I figured out I was getting this error because git@github.com was not added to SSH
+# So I had to use ssh -T git@github.com in the terminal 
+# Is this ok?
