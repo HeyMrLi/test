77ca0e93f1e18cf595edb220e6bfb7607127641a
Sean Li <reachsean@gmail.com>
2019-12-07 00:53:53 -0800

    Notes update

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ print (viking_song)
 
 print("Hello World")
 
-# Before I got this file to work, I had to re-clone using SSH key
+# Before I got this file to upload, I had to re-clone using SSH key
 # So next I had to learn how to create an SSH key on my Mac
 # After that, I installed the SSH key onto my GitHub
 # Then, I got stuck for 15-20 minutes getting an "Git: host key verification failed" error
