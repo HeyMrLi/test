59d4dca1cfb198fb33411ab58b188fdff137863a
Sean Li <reachsean@gmail.com>
2019-12-08 20:40:07 -0800

    python challenge

--- hello.py
+++ hello.py
@@ -19,3 +19,32 @@ print("Hello World")
 # I figured out I was getting this error because git@github.com was not added to SSH
 # So I had to use ssh -T git@github.com in the terminal 
 # Is this ok?
+
+# Codeacademy
+# I can't just put an equation 25 * 68 + 13 / 28, I need to print it.
+
+print(25 * 68 + 13 / 28)
+
+quilt_width = 8
+quilt_length = 12
+
+squares = quilt_width * quilt_length
+print("Will need: " + str(squares) + " squares.")
+
+quilt_length = 8
+squares = quilt_width * quilt_length
+print("Will need: " + str(squares) + " squares.")
+beg_text = "Will need:"
+end_text = "squares."
+print(beg_text, squares, end_text)
+
+team = []
+for i in range(4):
+    team.append([])
+for person in range(1, 29):
+    for i in range(4):
+        if person % 4 == i:
+            team[i].append(person)
+
+for i in range(4):
+    print("Team {} are persons {}".format(i, team[i]))   
