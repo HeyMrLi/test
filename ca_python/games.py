13f186de0a3a907dc668f23dc1db0a236f6c8694
Sean Li <reachsean@gmail.com>
2019-12-10 00:18:49 -0800

    Account for weird upper lower case input

--- ca_python/games.py
+++ ca_python/games.py
@@ -19,11 +19,14 @@ def coin_flip(guess,bet):
   result = random.randint(1, 2)
 
   if result == 1:
-    result = "Heads"
+    result = "heads"
     print("Coin Lands Heads!")
   else:
-    result = "Tails"
+    result = "tails"
     print ("Coin Lands Tails!")
+    
+  # Lowercase inputs
+  guess = guess.lower()
 
   if guess == result:
     print("You won your bet of $" + str(bet))
@@ -49,12 +52,15 @@ def cho_han(guess,bet):
   result = dice1 + dice2
   
   if result % 2 == 0:
-    result = "Even"
+    result = "even"
     print(str(dice1) + " + " + str(dice2) + " = " + result)
   else:
-    result = "Odd"
+    result = "odd"
     print(str(dice1) + " + " + str(dice2) + " = " + result)
 
+  # Lowercase inputs
+  guess = guess.lower()
+  
   if guess == result:
     print("You won your bet of $" + str(bet))
     return bet
@@ -105,6 +111,10 @@ def roulette(guess,bet):
   ball_lands = random.randint(0,37)
   print("Let's play Roulette!")
 
+  # Lowercase inputs
+  if isinstance(guess,str) == True:
+    guess = guess.lower()
+  
   # Win if number guess is equal to random Int 
   if guess == ball_lands and ball_lands != 37:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
@@ -116,31 +126,31 @@ def roulette(guess,bet):
     print("You win $" + str(bet * 35) + "!")
     return bet * 35
   # Win if bet Odds
-  elif guess == "Odd" and ball_lands % 2 != 0 and ball_lands != 37 and ball_lands != 0:
+  elif guess == "odd" and ball_lands % 2 != 0 and ball_lands != 37 and ball_lands != 0:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
     print(str(ball_lands) + " is an Odd number.")
     print("You win $" + str(bet) + "!")
     return bet
   # Win if bet Even
-  elif guess == "Even" and ball_lands % 2 == 0 and ball_lands != 0 and ball_lands != 37:
+  elif guess == "even" and ball_lands % 2 == 0 and ball_lands != 0 and ball_lands != 37:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
     print(str(ball_lands) + " is an Even number.")
     print("You win $" + str(bet) + "!")
     return bet
   # Win if bet First Third
-  elif guess == "First" and ball_lands / 3 <= 4 and ball_lands != 0 and ball_lands != 37:
+  elif guess == "first" and ball_lands / 3 <= 4 and ball_lands != 0 and ball_lands != 37:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
     print(str(ball_lands) + " is in the First Third.")
     print("You win $" + str(bet * 2) + "!")
     return bet * 2
   # Win if bet Second Third
-  elif guess == "Second" and ball_lands / 3 > 4 and ball_lands / 3 <= 8 and ball_lands != 0 and ball_lands != 37:
+  elif guess == "second" and ball_lands / 3 > 4 and ball_lands / 3 <= 8 and ball_lands != 0 and ball_lands != 37:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
     print(str(ball_lands) + " is in the First Third.")
     print("You win $" + str(bet * 2) + "!")
     return bet * 2
   # Win if bet Third Third
-  elif guess == "Third" and ball_lands / 3 > 8 and ball_lands != 0 and ball_lands != 37:
+  elif guess == "third" and ball_lands / 3 > 8 and ball_lands != 0 and ball_lands != 37:
     print("You guessed " + str(guess) + " and the ball landed on " + str(ball_lands) + "!")
     print(str(ball_lands) + " is in the First Third.")
     print("You win $" + str(bet * 2) + "!")
@@ -157,21 +167,16 @@ def roulette(guess,bet):
     return -bet
     
 #Calling game of chance functions here
-money += coin_flip("Heads",10)
+money += coin_flip("hEAds",10)
 money += cho_han("Even",10)
 money += game_of_war(10)
-money += roulette("First",10)
-money += roulette("Second",10)
+money += roulette("first",10)
+money += roulette("second",10)
 money += roulette("Third",10)
-money += roulette("Even",25)
-money += roulette("Odd",25)
-money += roulette(15,5)
-money += roulette(33,5)
-money += roulette(2,5)
-money += roulette(4,5)
-money += roulette(12,5)
+money += roulette("Even",30)
+money += roulette("Odd",30)
+money += roulette(15,10)
 money += roulette("00",5)
-money += roulette(0,5)
 print("-" * 40)
 print("Your ending total is $" + str(money))
 
